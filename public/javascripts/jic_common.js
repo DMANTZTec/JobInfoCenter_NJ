b9e8ca22182189dd23a0b8101b7d44607f7fd4c8
vandana <samudrala.vandana@gmail.com>
2017-11-16 10:42:38 +0530

    login screen added

--- public/javascripts/jic_common.js
+++ public/javascripts/jic_common.js
@@ -172,6 +172,7 @@ function validatelogin() {
 
     function onSuccess(googleUser) {
         document.getElementById('login1').style.display = 'none';
+        document.getElementById('btn').style.display = 'none';
         //document.getElementById('aClassOfYourOwn').style.display='block';
         var data = '';
         data += '<ul>';
@@ -207,11 +208,11 @@ function validatelogin() {
         //document.getElementById('gglg').setAttribute("onclick","onSignIn()");
         //document.getElementById('btn').style.display='none';
         //document.getElementById('aClassOfYourOwn').style.display="none";
-        document.getElementById('login1').style.display = "none";
+        //document.getElementById('login1').style.display = "none";
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             console.log('User signed out.');
-            document.getElementById('btn').style.display = "none";
+            document.getElementById('btn').style.display = "block";
 
         });
     }
